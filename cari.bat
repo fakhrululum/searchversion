c53efa8d8e69bc96012eef64aaf8a1f1bfa3e7db
Fakhrul Ulum DM <fakhrul.ulum@gmail.com>
2024-09-24 13:58:54 +0800

    Update cari.bat

--- cari.bat
+++ cari.bat
@@ -6,6 +6,11 @@ for /f "tokens=*" %%a in ('dir *TRANS.exe* /s /b') do (
     set "filepath=%%a"
     set "file=%%a"
     set "filepath=!filepath:\=\\!"
-    wmic datafile where "name='!filepath!'" get Version /value | find "2024.8.0"
-    echo !file!
+    ::wmic datafile where "name='!filepath!'" get Version /value | find "2024.8.0"
+    for /f "tokens=2 delims==" %%b in ('wmic datafile where "name='!filepath!'" get Version /value ^| find "="') do (
+        set "version=%%b"
+        if "!version!"=="2024.8.0" echo Filepath: %%a
+    )
 )
+
+endlocal

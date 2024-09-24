9a0404e05c489b2ef04293be9616bd7eb1db0a5b
Fakhrul Ulum DM <fakhrul.ulum@gmail.com>
2024-09-24 13:32:10 +0800

    Update cari.bat

--- cari.bat
+++ cari.bat
@@ -4,6 +4,8 @@ setlocal enabledelayedexpansion
 
 for /f "tokens=*" %%a in ('dir *TRANS.exe* /s /b') do (
     set "filepath=%%a"
+    set "file=%%a"
     set "filepath=!filepath:\=\\!"
-    wmic datafile where "name='!filepath!'" get Version | find "2024.4.0" 
+    wmic datafile where "name='!filepath!'" get Version | find "2024.8.0"
+    echo !file!
 )
